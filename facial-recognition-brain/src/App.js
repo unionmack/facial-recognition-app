d6652090b04130dc966474fd2f02cf5da205bc8a
Mack Hayden <unionmack@gmail.com>
2020-06-15 16:40:04 -0700

    added tags for main four components in App.js

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
     </div>
   );
 }
