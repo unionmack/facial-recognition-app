10a63ab9eb77e5d9a8e97c898271e7af674a8c09
Mack Hayden <unionmack@gmail.com>
2020-06-15 16:54:13 -0700

    added basic styling to Navigation component

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      // {/*<Logo />
+      {/*<Logo />
       // <ImageLinkForm />
   // <FaceRecognition />*/}
     </div>
