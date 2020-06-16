09c995260c92cee9841bb7e3abe898f2721211aa
Mack Hayden <unionmack@gmail.com>
2020-06-16 16:05:48 -0700

    added in particles npm and component

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -4,10 +4,26 @@ import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import Rank from './Components/Rank/Rank.js'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js'
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles"
+              params={particlesOptions}
+            />
       <Navigation />
       <Logo />
       <Rank />
