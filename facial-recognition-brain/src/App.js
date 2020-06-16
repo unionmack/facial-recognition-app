c7b080e28de6e5c7855ebd7b1195af9a4e626d2b
Mack Hayden <unionmack@gmail.com>
2020-06-16 15:08:12 -0700

    started created Logo.js component and imported React-Tilt npm

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation'
+import Logo from './Components/Logo/Logo'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
       {/*<Logo />
       // <ImageLinkForm />
   // <FaceRecognition />*/}
---
+++ facial-recognition-brain/src/Components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+        
+        </div>
+    );
+}
+
+export default Logo;
