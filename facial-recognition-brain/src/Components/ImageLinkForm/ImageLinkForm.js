829e327476730dba6fd78d053d95272b201e8ced
Mack Hayden <unionmack@gmail.com>
2020-06-16 15:45:11 -0700

    added CSS pattern to ImageLinkForm

--- facial-recognition-brain/src/Components/ImageLinkForm/ImageLinkForm.js
+++ facial-recognition-brain/src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
     return (
@@ -7,8 +8,10 @@ const ImageLinkForm = () => {
                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
         </p>
         <div className="center">
-                <input className='f4 pa2 w-70 center' type='tex' />
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                <div className="pa4 br3 shadow-5 form center">
+                    <input className='f4 pa2 w-70 center' type='tex' />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
         </div>
        </div>
     );
