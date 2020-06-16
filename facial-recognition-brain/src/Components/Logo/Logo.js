bcca250938c4e7c86a3f740a50b12cf66f0449ee
Mack Hayden <unionmack@gmail.com>
2020-06-16 15:22:26 -0700

    added brain icon and finished styling Tilt Logo for now

--- facial-recognition-brain/src/Components/Logo/Logo.js
+++ facial-recognition-brain/src/Components/Logo/Logo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css'
+import brain from './brain-icon.png'
 
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> 👽 </div>
+                <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}} src={brain}></img> </div>
         </Tilt>
         </div>
     );
