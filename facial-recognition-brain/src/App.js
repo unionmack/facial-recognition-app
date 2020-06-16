5f078f53e2e3f0f179f46fc305e5555671640ea8
Mack Hayden <unionmack@gmail.com>
2020-06-16 15:24:12 -0700

    began working on ImageLinkForm component

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
+import ImageLinkForm from '.Components/ImageLinkForm/ImageLinkForm'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <ImageLinkForm />
       {/*<Logo />
       // <ImageLinkForm />
   // <FaceRecognition />*/}
