a1af4efa88c0538d6ff00d624c8b4c9070054d7f
Mack Hayden <unionmack@gmail.com>
2020-06-18 19:08:35 -0700

    added onButtonSubmit to signal when user clicks the Detect button

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -32,6 +32,10 @@ class App extends Component {
     console.log(event.target.value);
   }
 
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,7 +45,7 @@ class App extends Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm onInputChange={this.onInputChange}/>
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       {/*<Logo />
       // <ImageLinkForm />
   // <FaceRecognition />*/}
