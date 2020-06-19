fe9575e9454bbec8b09cc45229f094b8157e55eb
Mack Hayden <unionmack@gmail.com>
2020-06-18 19:40:08 -0700

    update to imageUrl functionality

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -36,12 +36,12 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.COLOR_MODEL, "https://samples.clarifai.com/face-det.jpg").then(
+    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
     function(response) {
       // do something with response
       console.log(response);
