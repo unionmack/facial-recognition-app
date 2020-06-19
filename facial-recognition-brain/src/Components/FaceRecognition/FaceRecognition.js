bb6e052cfada2c1f615f14a3e0e78ac59f9656ae
Mack Hayden <unionmack@gmail.com>
2020-06-18 19:47:07 -0700

    provided styling to ensure images are all appropriately sized

--- facial-recognition-brain/src/Components/FaceRecognition/FaceRecognition.js
+++ facial-recognition-brain/src/Components/FaceRecognition/FaceRecognition.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FaceRecognition = ({ imageUrl }) => {
     return (
-        <div className="center">
-            <img src={imageUrl}></img>
+        <div className="center ma">
+            <div className="absolute mt2">
+                <img src={imageUrl} width='500px' height="auto"></img>
+            </div>
         </div>
     )
 }
