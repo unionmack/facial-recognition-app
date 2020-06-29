1570b2b201af9339d46716646d1052677f2ee262
Mack Hayden <unionmack@gmail.com>
2020-06-29 16:38:11 -0700

    destructured App.js

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -84,23 +84,24 @@ class App extends Component {
   }
 
   render() {
+    const {isSignedIn, imageUrl, route, boxAll} = this.state;
     return (
       <div className="App">
       <Particles className="particles"
               params={particlesOptions}
             />
-      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
-      {this.state.route === 'home' 
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      {route === 'home' 
         ? <div>
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         <FaceRecognition 
-        imageUrl={this.state.imageUrl} 
-        boxAll={this.state.boxAll}/> 
+        imageUrl={imageUrl} 
+        boxAll={boxAll}/> 
       </div>
         : (
-          this.state.route === "signin"
+          route === "signin"
           ? <SignIn onRouteChange={this.onRouteChange}/>
           : <Register onRouteChange={this.onRouteChange}/>
         )
