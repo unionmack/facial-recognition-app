35f2ca32480d9a700b400a419a5f01b430ddbbc9
Mack Hayden <unionmack@gmail.com>
2020-07-12 15:35:58 -0700

    added smart SignIn features to SignIn component

--- facial-recognition-brain/src/Components/SignIn/SignIn.js
+++ facial-recognition-brain/src/Components/SignIn/SignIn.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const SignIn = ({onRouteChange}) => {
-    return (
+class SignIn extends React.Component {
+   constructor(props) {
+       super(props);
+       this.state = {
+           signInEmail: '',
+           signInPassword: ''
+       }
+   }
+   
+    onEmailChange = (event) => {
+        this.setState({signInEmail: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({signInPassword: event.target.value})
+    }
+
+    onSubmitSignIn = () => {
+        console.log(this.state)
+    }
+
+    render(){
+        const {onRouteChange} = this.props;
+        return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
             <div className="measure">
@@ -29,7 +51,7 @@ const SignIn = ({onRouteChange}) => {
             </div>
       </main>
       </article>
-    )
+    )}
 }
 
 export default SignIn;
