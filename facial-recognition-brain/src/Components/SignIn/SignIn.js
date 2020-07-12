a748a573ed7442f112b4bd539986b6088b003c11
Mack Hayden <unionmack@gmail.com>
2020-07-12 16:02:24 -0700

    added functionality so user can now enter email and password to log in and set state

--- facial-recognition-brain/src/Components/SignIn/SignIn.js
+++ facial-recognition-brain/src/Components/SignIn/SignIn.js
@@ -18,8 +18,20 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        console.log(this.state);
-        this.props.onRouteChange('home');
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data==='success') {
+                this.props.onRouteChange('home');
+            }
+        })
     }
 
     render(){
