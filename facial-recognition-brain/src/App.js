d61b6d0f6f9be558d4f7909c1c9229f3bdb91343
Mack Hayden <unionmack@gmail.com>
2020-07-21 16:03:26 -0700

    improved error handling

--- facial-recognition-brain/src/App.js
+++ facial-recognition-brain/src/App.js
@@ -113,6 +113,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
